0f5387bd98488bfcce0cb27d0a650722a1f00c2e
Rob Herley <robert.herley@ibm.com>
2017-06-16 20:47:47 -0400

    added a script to download ui

---
+++ README.md
@@ -0,0 +1,1 @@
+ Simple Node.js server that pulls the ui from the [Github repo](https://github.com/robherley/robherley-ui) and serves it.
--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('💾 Website is running on port:', app.get('port'));
 });
